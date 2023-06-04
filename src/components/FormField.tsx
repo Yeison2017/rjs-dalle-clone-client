a7dc0456b7d758816090d856a8f6f61579ff0598
yeison2017 <yha.big@gmail.com>
2023-06-04 17:55:31 -0500

    Se maqueta el formulario para crear la imagen

---
+++ src/components/FormField.tsx
@@ -0,0 +1,55 @@
+interface Props {
+  labelName: string;
+  type: string;
+  name: string;
+  placeholder: string;
+  value: string;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  isSurpiseMe?: boolean;
+  handleSurpriseMe?: () => void;
+}
+
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpiseMe,
+  handleSurpriseMe,
+}: Props) => {
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {labelName}
+        </label>
+        {isSurpiseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
+          >
+            Surprise me
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
+      />
+    </div>
+  );
+};
+
+export default FormField;
---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+import { surpriseMePrompts } from "../constants";
+
+export const getRandomPrompt = (prompt: string): string => {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPrompt: string = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+  return randomPrompt;
+};
